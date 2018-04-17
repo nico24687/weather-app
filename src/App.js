2f5a8835e0f487cab80e56824f8c2c45c2ce08a1
nico24687 <theonlynico@hotmail.com>
2018-04-17 13:45:14 -0600

    final commit

--- src/App.js
+++ src/App.js
@@ -24,7 +24,6 @@ export default class App extends Component {
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`)
     const data = await api_call.json()
     if(city && country){
-      console.log(data)
       this.setState({
         temperature: data.main.temp,
         city: data.name,
@@ -47,17 +46,31 @@ export default class App extends Component {
   render(){
     return(
       <div>
-        <Titles/>
-        <Form getWeather={this.getWeather}/>
-        <Weather 
-          temperature={this.state.temperature}
-          city={this.state.city}
-          country={this.state.country}
-          humidity={this.state.humidity}
-          description={this.state.description}
-          error={this.state.error}
-        />
+        <div className="wrapper">
+          <div className="main">
+            <div className="container">
+              <div className="row">
+                <div className="col-xs-5 title-container">
+                  <Titles />
+                </div>
+                <div className="col-xs-7 form-container">
+                  <Form getWeather={this.getWeather} />
+                  <Weather
+                    temperature={this.state.temperature}
+                    city={this.state.city}
+                    country={this.state.country}
+                    humidity={this.state.humidity}
+                    description={this.state.description}
+                    error={this.state.error}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
 }
+
+
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -3,12 +3,30 @@ import React, {Component} from 'react'
 export default class Weather extends Component{
   render(){
     return(
-      <div>
-        { this.props.city && this.props.country && <p>Location: {this.props.city}, {this.props.country}</p> } 
-        { this.props.temperature && <p>Temperature: {this.props.temperature}</p>}
-        { this.props.humidity && <p>Humidity: {this.props.humidity}</p>}
-        { this.props.description && <p>Description: {this.props.description}</p> }
-        {this.props.error && <p>{this.props.error}</p>}
+      <div className="weather__info">
+        { 
+          this.props.city && this.props.country && <p className="weather__key">Location: 
+            <span className="weather__value"> {this.props.city}, {this.props.country} </span>
+          </p> 
+        } 
+        { 
+          this.props.temperature && <p className="weather__key">Temperature: 
+            <span className="weather__value"> {this.props.temperature} </span>
+          </p>
+        }
+        { 
+          this.props.humidity && <p className="weather__key">Humidity: 
+            <span className="weather__value"> {this.props.humidity} </span>
+          </p>
+        }
+        { 
+          this.props.description && <p className="weather__key">Description: 
+            <span className="weather__value"> {this.props.description} </span>
+          </p> 
+        }
+        {
+          this.props.error && <p className="weather__error">{this.props.error}</p>
+        }
       </div>
     )
   }
