d98f0683232207ebded0071b93fe8209d2cddd7d
nico24687 <theonlynico@hotmail.com>
2018-04-17 10:25:50 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+export default class App extends Component {
+  render(){
+    return(
+      <div>
+        <h1>Should display this</h1>
+        <p>This displays to as it is wrapped in a parent component</p>
+      </div>
+    )
+  }
+}
